1c53bf9579f12d5c211eb77e456cd823ffbd937c
louga29 <105203351+louga29@users.noreply.github.com>
2025-02-03 09:04:51 +0100

    Update app.py

--- flask/app.py
+++ flask/app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 pipeline_path = "models/text_classification_pipeline.pkl"
 pipeline = joblib.load(pipeline_path)
 
-uri = "mongodb+srv://arthurgautier29480:lapin@cluster0.wuq03.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+uri = "mongodb+srv://arthurgautier29480:Fortin.12@cluster0.wuq03.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 client = MongoClient(uri, server_api=ServerApi('1'))
 mydb = client.get_database("sentiment_analyser")
 collection = mydb["reviews"]
